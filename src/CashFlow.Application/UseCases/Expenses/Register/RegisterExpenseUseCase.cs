890bc692a2952676aa4328803f1e374791069b1e
leandroslalves <leandro.usl@live.com>
2025-01-09 10:13:27 -0300

    Estudando validações simples com if

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,44 @@
+﻿using CashFlow.Comunication.Enums;
+using CashFlow.Comunication.Requests;
+using CashFlow.Comunication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponseRegisteredExpenseJson Execute(RequestRegisterExepensesJson request)
+        {
+            Validate(request);
+
+            return new ResponseRegisteredExpenseJson();
+        }
+
+
+        private void Validate(RequestRegisterExepensesJson request)
+        {
+            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+            if (titleIsEmpty)
+            {
+                throw new ArgumentException("Title is required");
+            }
+
+            if (request.Amount <= 0)
+            {
+                throw new ArgumentException("Amount must be greater than zero");
+            }
+
+            var result = DateTime.Compare(request.Date, DateTime.Now);
+            if (result > 0)
+            {
+                throw new ArgumentException("Date must be less than or equal to today");
+            }
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+            if (!paymentTypeIsValid)
+            {
+                throw new ArgumentException("Payment type is invalid");
+            }
+
+        }
+    }
+}
---
+++ src/CashFlow.Comunication/Responses/ResponseRegisteredExpenseJson.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Comunication.Responses
+{
+    public class ResponseRegisteredExpenseJson
+    {
+        public string Title { get; set; } = string.Empty;
+
+    }
+}
