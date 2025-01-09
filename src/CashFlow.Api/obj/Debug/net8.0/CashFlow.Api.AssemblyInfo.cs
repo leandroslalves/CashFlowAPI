00fe07ba5bbce8b8e77ef1ba6cfb53090adff93f
leandroslalves <leandro.usl@live.com>
2025-01-09 08:18:07 -0300

    Exclusão dos controllers desnecessários

--- src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a4fa803c944f7f16cc8d47342e384274365ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
